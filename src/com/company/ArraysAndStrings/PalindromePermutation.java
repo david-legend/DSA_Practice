26f699a0fa54a92b565ce6c0c1b3fa55d4ce979c
David <davidcobbina47@gmail.com>
2021-01-18 11:07:07 +0200

    added solution for palindrome and URLify

--- src/com/company/ArraysAndStrings/PalindromePermutation.java
+++ src/com/company/ArraysAndStrings/PalindromePermutation.java
@@ -6,8 +6,62 @@ package com.company.ArraysAndStrings;
 // A palindrome is a word or phrase that is the same forwards and backwards.
 // A permutation is a rearrangement of letters.
 // The palindrome does not need to be limited to just dictionary words.
+
 public class PalindromePermutation {
     public static void main(String[] args) {
-        // write your code here
+        String[] palindromes = {
+                "madam", "peace", "civic", "algorithm", "racecar", "radar",
+                "hannah", "stats", "tenet", "Kayak", "level"
+        };
+
+        System.out.println(isPermutationPalindrome(palindromes[0]));
+        System.out.println(isPermutationPalindrome(palindromes[1]));
+        System.out.println(isPermutationPalindrome(palindromes[2]));
+        System.out.println(isPermutationPalindrome(palindromes[3]));
+        System.out.println(isPermutationPalindrome(palindromes[4]));
+    }
+
+    public static boolean isPermutationPalindrome(String phrase) {
+        int[] table = buildCharFrequencyTable(phrase);
+
+        return checkMaxOneOdd(table);
+    }
+
+    public static boolean checkMaxOneOdd(int[] table) {
+        boolean foundOdd = false;
+        for (int count : table) {
+            if (count % 2 == 1) {
+                if (foundOdd) {
+                    return false;
+                }
+                foundOdd = true;
+            }
+        }
+        return true;
+    }
+
+    public static int getCharNumber(Character c) {
+        int a = Character.getNumericValue('a');
+        int z = Character.getNumericValue('z');
+        int val = Character.getNumericValue(c);
+
+        if (a <= val && val <= z) {
+            return val - a;
+        }
+
+        return -1;
+    }
+
+
+    public static int[] buildCharFrequencyTable(String phrase) {
+        int[] table = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
+
+        for (char c : phrase.toCharArray()) {
+            int x = getCharNumber(c);
+            if (x != -1) {
+                table[x]++;
+            }
+        }
+        return table;
     }
 }
--- src/com/company/ArraysAndStrings/URLify.java
+++ src/com/company/ArraysAndStrings/URLify.java
@@ -8,11 +8,57 @@ package com.company.ArraysAndStrings;
 // (Note: If implementing in Java,please use a character array so that you can perform this operation in place.)
 
 //EXAMPLE
-//Input: "Mr John Smith ", 13
+//Input: "Mr John Smith    ", 13
 // Output: "Mr%20John%20Smith"
+
+//TODO:: Understand solution for the question very well
 public class URLify {
 
     public static void main(String[] args) {
         // write your code here
+        String[] test1 = {"Mr John Smith    ", "13"};
+        String[] test2 = {"We can walk home      ", "16"};
+        System.out.println(solution(test1[0], Integer.parseInt(test1[1])));
+        System.out.println(solution(test2[0], Integer.parseInt(test2[1])));
+
+//        replaceSpaces(test1[0].toCharArray(), Integer.parseInt(test1[1]));
+//        replaceSpaces(test2[0].toCharArray(), Integer.parseInt(test2[1]));
+    }
+
+
+    //My Solution
+    public static String solution(String input, int length) {
+        String actual = input.substring(0, length);
+        String val = actual.replace(" ", "%20");
+        return val;
+
+    }
+
+    //Solution from book
+    static void replaceSpaces(char[] str, int trueLength) {
+
+        int spaceCount = 0, index, i = 0;
+        for (i = 0; i < trueLength; i++) {
+            if (str[i] == ' ') {
+                spaceCount++;
+            }
+        }
+
+        index = trueLength + spaceCount * 2;
+        if (trueLength < str.length) str[trueLength] = '\0';
+
+        for (i = trueLength - 1; i >= 0; i--) {
+            if (str[i] == ' ') {
+                str[index - 1] = '0';
+                str[index - 2] = '2';
+                str[index - 3] = '%';
+
+            } else {
+                str[index - 1] = str[i];
+                index--;
+            }
+        }
+
+        System.out.println(new String(str));
     }
 }
